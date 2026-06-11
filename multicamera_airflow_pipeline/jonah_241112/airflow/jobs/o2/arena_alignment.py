5195cf4fb00935c5b357980b07a6735425444633
Jonah Pearl <jonahpearl@g.harvard.edu>
2026-06-11 18:17:12 -0400

    fix missed line from old improvement

--- multicamera_airflow_pipeline/jonah_241112/airflow/jobs/o2/arena_alignment.py
+++ multicamera_airflow_pipeline/jonah_241112/airflow/jobs/o2/arena_alignment.py
@@ -55,7 +55,7 @@ def arena_alignment(
 
     # check if sync successfully completed
     from multicamera_airflow_pipeline.jonah_241112.airflow.dag_o2 import dummy_dag
-    downstream_tasks = dummy_dag.get_all_downstream_tasks(recording_row.overwrite_from)
+    downstream_tasks = dummy_dag.get_all_downstream_tasks(recording_row.overwrite_from) | set([recording_row.overwrite_from])
     if not recording_row.overwrite or (recording_row.overwrite and ("arena_alignment" not in downstream_tasks)):
         if check_arena_alignment_completion(arena_alignment_output_directory):
             logger.info("arena_alignment completed, quitting")
